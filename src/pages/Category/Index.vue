7d9e2e156d61ced5182c161f4c40865b332973ea
ZoDream <648383079@qq.com>
2019-03-13 22:15:45 +0800

    修改

--- src/pages/Category/Index.vue
+++ src/pages/Category/Index.vue
@@ -1,55 +1,49 @@
 <template>
     <div>
+        <header class="top">
+            <a class="search-entry" @click="$router.push('/search')">
+                <i class="fa fa-search"></i>
+                <span>搜索商品, 共19304款好物</span>
+            </a>
+        </header>
         <div class="has-header has-footer category-page">
             <div class="category-menu">
-                <?php foreach($cat_list as $item):?>
-                <div class="menu-item"><?=$item->name?></div>
-                <?php endforeach;?>
+                <div v-for="(item, index) in categories" :key="index" :class="['menu-item', item.id == category.id ? 'active' : '']" @click="tapSelected(item)">{{ item.name }}</div>
             </div>
 
-            <div class="category-main">
-                <?php foreach($cat_list as $item):?>
-                <div class="item active lazy-loading" data-url="<?=$this->url('./mobile/category/children', ['id' => $item->id])?>">
+            <div class="category-main" v-if="category">
+                <div class="item active">
                     <div class="banner">
-                        <img src="<?=$category->app_banner?>" alt="">
+                        <img :src="category.banner" alt="">
                     </div>
                     <div class="header">
-                        <a href="<?=$this->url('./mobile/search', ['cat_id' => $category->id])?>"><?=$category->name?></a>
+                        <a @click="tapSearch(category)">{{ category.name }}</a>
                     </div>
-                    <div class="goods-list">
-                        <?php foreach($hot_list as $item):?>
-                        <div class="item-view">
+                    <div class="goods-list" v-if="category.goods_list && category.goods_list.length > 0">
+                        <div class="item-view" v-for="(item, index) in category.goods_list" :key="index">
                             <div class="item-img">
-                                <a href="<?=$this->url('./mobile/goods', ['id' => $item->id])?>"><img src="<?=$item->thumb?>" alt=""></a>
+                                <a  @click="tapProduct(item)"><img :src="item.thumb" alt=""></a>
                             </div>
                             <div class="item-title">
-                                <?=$item->name?>
+                                {{item.name}}
                             </div>
                             <div class="item-actions">
-                                <span class="item-price"><?=$item->price?></span>
+                                <span class="item-price">{{ item.price }}</span>
                                 
                             </div>
                         </div>
-                        <?php endforeach;?>
                     </div>
-                    <ul class="tree-grid">
-                        <?php foreach($category_tree as $item):?>
-                        <li class="tree-item">
-                            <a href="<?=$this->url('./mobile/search', ['cat_id' => $item['id']])?>"><?=$item['name']?></a>
-                            <?php if(isset($item['children'])):?>
-                            <ul class="tree-item-chidren">
-                                <?php foreach($item['children'] as $it):?>
-                                <li class="tree-item">
-                                    <a href="<?=$this->url('./mobile/search', ['cat_id' => $it['id']])?>"><?=$it['name']?></a>
-                                </li> 
-                                <?php endforeach;?>
+                    <ul class="tree-grid" v-if="category.children && category.children.length > 0">
+                        <li class="tree-item" v-for="(item, index) in category.children" :key="index">
+                            <a  @click="tapSearch(item)">{{ item.name }}</a>
+                            <ul class="tree-item-chidren" v-if="item.children && item.children.length > 0">
+                                <li class="tree-item" v-for="(it, i) in item.children" :key="i">
+                                    <a  @click="tapSearch(it)">{{ it.name }}</a>
+                                </li>
                             </ul>
-                            <?php endif;?>
                         </li>
-                        <?php endforeach;?>
                     </ul>
                 </div>
-                <?php endforeach;?>
             </div>
 
         </div>
@@ -57,10 +51,25 @@
 </template>
 <script lang="ts">
 import { Vue, Component, Prop, Emit } from 'vue-property-decorator';
+import { ICategory, IProduct } from '@/api/model';
 
 @Component
 export default class Index extends Vue {
+    categories: ICategory[] = [];
 
+    category: ICategory | null = null;
+
+    tapSelected(item: ICategory) {
+        this.category = item;
+    }
+
+    tapSearch(item: ICategory) {
+        this.$router.push({name: 'search', query: {category: item.id + ''}});
+    }
+
+    tapProduct(item: IProduct) {
+        this.$router.push({name: 'product', params: {id: item.id + ''}});
+    }
 }
 </script>
 <style lang="scss" scoped>
