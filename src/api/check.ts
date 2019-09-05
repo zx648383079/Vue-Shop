599c4cc3c4d03ee167913faebe7572361dbaf68b
ZoDream <648383079@qq.com>
2019-09-05 22:51:10 +0800

    修改签到链接

--- src/api/check.ts
+++ src/api/check.ts
@@ -1,6 +1,6 @@
 import { IDataOne, IData, ICheckIn } from './model';
 import { fetch, post } from '@/utils/http';
 
-export const getCanCheckIn = () => fetch<IDataOne<ICheckIn>>('auth/check_in');
-export const checkIn = () => post<IDataOne<ICheckIn>>('auth/check_in/check_in');
-export const getMonth = (month?: string) => post<IData<ICheckIn>>('auth/check_in/month', {month});
+export const getCanCheckIn = () => fetch<IDataOne<ICheckIn>>('check_in');
+export const checkIn = () => post<IDataOne<ICheckIn>>('check_in/home/check_in');
+export const getMonth = (month?: string) => post<IData<ICheckIn>>('check_in/home/month', {month});
